5388f66b3912acf01bc39775af022a33f00d8a23
afnylatifa <afnylatifa@gmail.com>
2023-12-01 20:56:47 +0700

    membuat tamplate main

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,80 @@
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+      <!-- Section: Links  -->
+      <section class="container-fluid" style="background-color: #3570d6">
+        <div class="container text-center text-white text-md-start mt-5 pt-2">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <!-- Content -->
+              <img src="img/logo-lapak-telu.png" alt="" width="40%" class="img-fluid mb-3" />
+              <p class="fs-2">Lapak Tel-U</p>
+              <p>Lapak Telkom University merupakan website penyedia forum jual beli untuk membantu menjualbelikan barang bekas (second/preloved) bagi seluruh civitas akademik Telkom University, Bandung.</p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Layanan</h6>
+              <p>
+                <a href="#!" class="text-reset text-decoration-none">Jual</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset text-decoration-none">Beli</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset text-decoration-none">Tanya</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset text-decoration-none">Saran</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Link</h6>
+              <p>
+                <a href="#!" class="text-reset text-decoration-none">About Us</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset text-decoration-none">Settings</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset text-decoration-none">Pesanan</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset text-decoration-none">Bantuan</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Kontak</h6>
+              <p><i class="fas fa-home me-3"></i>TULT Lantai 6</p>
+              <p>
+                <i class="fas fa-envelope me-3"></i>
+                info@lapaktelu.com
+              </p>
+              <p><i class="fas fa-phone me-3"></i> + 221 476373</p>
+              <p><i class="fas fa-print me-3"></i> + 221 834980</p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05)">
+        © 2023 Copyright:
+        <a class="text-reset fw-bold" href="#">www.lapaktelu.com</a>
+      </div>
+      <!-- Copyright -->
+    </footer>
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid justify-content-center">
             <!-- logo -->
@@ -55,5 +48,3 @@
             </div>
         </div>
     </nav>
-</body>
-</html>
